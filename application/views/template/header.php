6a2d0771419f672b89da584a04a2b5a402a65e3b
johannkaltner <johannkaltnerdev@gmail.com>
2019-11-06 17:09:14 -0300

    logo adicionada + get date no mobile header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -47,13 +47,13 @@
 			<div class="section__content section__content--p35">
 				<div class="header3-wrap">
 					<div class="header__logo">
-						<a href="#">
-							<h1 style="color:#2C0CB8"> RF&A </h1>
-							<!-- <img src="<?php echo base_url(); ?>public/images/icon/logo.png" alt="" /> -->
+						<a href="<?php echo base_url();?>home">
+							<!-- <h1 style="color:#2C0CB8"> RF&A </h1> -->
+							<img src="<?php echo base_url(); ?>public/images/icon/logo_completa.png" alt="" />
 						</a>
 					</div>
 					<div class="header__navbar">
-						<ul class="list-unstyled">
+						<ul class="list-unstyled" style="padding-top:30px;">
 							<li class="has-sub">
 								<a href="<?php echo base_url(); ?>home">
 									<i class="fas fa-home"></i>Dashboard
@@ -282,31 +282,31 @@
 			<div class="header-mobile__bar">
 				<div class="container-fluid">
 					<div class="header-mobile-inner">
-						<a href="<?php echo base_url();?> home">
-							<h1 style="color:#2C0CB8"> RF&A </h1>
-							<!-- <img src="<?php echo base_url(); ?>public/images/icon/logo.png" alt="" /> -->
+						<a href="<?php echo base_url();?>home">
+							<!-- <h1 style="color:#2C0CB8"> RF&A </h1> -->
+							<img src="<?php echo base_url(); ?>public/images/icon/logo_completa.png" alt="" />
 						</a>
 						
 						</a>
-						<button class="hamburger hamburger--slider" type="button">
-							<span class="hamburger-box ">
+						<button class="hamburger hamburger--slider"  type="button">
+							<span class="hamburger-box" ">
 								<span class="hamburger-inner" ></span>
 							</span>
 						</button>
 					</div>
 				</div>
 			</div>
-			<nav class="navbar-mobile">
+			<nav class="navbar-mobile"  style="padding-top:20px;">
 				<div class="container-fluid">
-					<ul class="navbar-mobile__list list-unstyled">
+					<ul class="navbar-mobile__list list-unstyled" >
 						<li class="has-sub">
 							<a class="js-arrow" style="color:black;" href="#">
 								<i class="fas fa-tachometer-alt"></i>Dashboard</a>
 							<ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
 								<li>
-									<a href="index.html">Dashboard 1</a>
+									<a href="<?php echo base_url();?>/home">Dashboard</a>
 								</li>
-								<li>
+								<!-- <li>
 									<a href="index2.html">Dashboard 2</a>
 								</li>
 								<li>
@@ -314,10 +314,10 @@
 								</li>
 								<li>
 									<a href="index4.html">Dashboard 4</a>
-								</li>
+								</li> -->
 							</ul>
 						</li>
-						<li>
+						<!-- <li>
 							<a href="chart.html">
 								<i class="fas fa-chart-bar"></i>Charts</a>
 						</li>
@@ -351,8 +351,8 @@
 									<a href="forget-pass.html">Forget Password</a>
 								</li>
 							</ul>
-						</li>
-						<li class="has-sub">
+						</li> -->
+						<!-- <li class="has-sub">
 							<a class="js-arrow" href="#">
 								<i class="fas fa-desktop"></i>UI Elements</a>
 							<ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
@@ -388,7 +388,7 @@
 								</li>
 								<li>
 									<a href="typo.html">Typography</a>
-								</li>
+								</li> -->
 							</ul>
 						</li>
 					</ul>
@@ -400,39 +400,19 @@
 				<div class="header-button-item has-noti js-item-menu">
 					<i class="zmdi zmdi-notifications"></i>
 					<div class="notifi-dropdown notifi-dropdown--no-bor js-dropdown">
-						<div class="notifi__title">
-							<p>You have 3 Notifications</p>
-						</div>
+					 
 						<div class="notifi__item">
 							<div class="bg-c1 img-cir img-40">
 								<i class="zmdi zmdi-email-open"></i>
 							</div>
 							<div class="content">
-								<p>You got a email notification</p>
-								<span class="date">April 12, 2018 06:50</span>
+								<p>Bem vindo ao novo CRM da RF&A!!</p>
+								<span class="date"><?php
+								    date_default_timezone_set('America/Sao_Paulo');
+								echo date('H:i:s / d-m-Y ');?></span>
 							</div>
 						</div>
-						<div class="notifi__item">
-							<div class="bg-c2 img-cir img-40">
-								<i class="zmdi zmdi-account-box"></i>
-							</div>
-							<div class="content">
-								<p>Your account has been blocked</p>
-								<span class="date">April 12, 2018 06:50</span>
-							</div>
-						</div>
-						<div class="notifi__item">
-							<div class="bg-c3 img-cir img-40">
-								<i class="zmdi zmdi-file-text"></i>
-							</div>
-							<div class="content">
-								<p>You got a new file</p>
-								<span class="date">April 12, 2018 06:50</span>
-							</div>
-						</div>
-						<div class="notifi__footer">
-							<a href="#">All notifications</a>
-						</div>
+						 
 					</div>
 				</div>
 				<div class="header-button-item js-item-menu">
@@ -498,9 +478,9 @@
 								</div>
 								<div class="content">
 									<h5 class="name">
-										<a href="#">Johann Kaltner</a>
+										<a href="#"><?php echo $this->session->userdata('usuario_nome');?></a>
 									</h5>
-									<span class="email">johannkaltnerdev@gmail.com</span>
+									<span class="email"><?php echo $this->session->userdata('usuario_email');?> </span>
 								</div>
 							</div>
 							<div class="account-dropdown__body">
