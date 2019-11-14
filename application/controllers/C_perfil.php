812e9fa54b992b42ccdeb5d2ff28eee53121b41c
johannkaltner <johannkaltnerdev@gmail.com>
2019-11-14 16:45:09 -0300

    model e controller criados para perfil e configuração

---
+++ application/controllers/C_perfil.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class C_Perfil extends CI_Controller {
+
+
+	public function index()
+	{
+		$this->template->show('perfil');	
+	}
+}
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -14,26 +14,21 @@
 
 	<!-- Fontfaces CSS-->
 	<link href="<?php echo base_url(); ?>public/css/font-face.css" rel="stylesheet" media="all">
-	<link href="<?php echo base_url(); ?>public/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet"
-		media="all">
-	<link href="<?php echo base_url(); ?>public/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet"
-		media="all">
-	<link href="<?php echo base_url(); ?>public/vendor/mdi-font/css/material-design-iconic-font.min.css"
-		rel="stylesheet" media="all">
+	<link href="<?php echo base_url(); ?>public/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+	<link href="<?php echo base_url(); ?>public/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+	<link href="<?php echo base_url(); ?>public/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
 	<!-- Bootstrap CSS-->
 	<link href="<?php echo base_url(); ?>public/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
 
 	<!-- Vendor CSS-->
 	<link href="<?php echo base_url(); ?>public/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-	<link href="<?php echo base_url(); ?>public/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css"
-		rel="stylesheet" media="all">
+	<link href="<?php echo base_url(); ?>public/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
 	<link href="<?php echo base_url(); ?>public/vendor/wow/animate.css" rel="stylesheet" media="all">
 	<link href="<?php echo base_url(); ?>public/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
 	<link href="<?php echo base_url(); ?>public/vendor/slick/slick.css" rel="stylesheet" media="all">
 	<link href="<?php echo base_url(); ?>public/vendor/select2/select2.min.css" rel="stylesheet" media="all">
-	<link href="<?php echo base_url(); ?>public/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet"
-		media="all">
+	<link href="<?php echo base_url(); ?>public/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
 
 	<!-- Main CSS-->
 	<link href="<?php echo base_url(); ?>public/css/theme.css" rel="stylesheet" media="all">
@@ -41,13 +36,14 @@
 </head>
 
 <body class="animsition">
+<?php date_default_timezone_set('America/Sao_Paulo'); ?>
 	<div class="page-wrapper">
 		<!-- HEADER DESKTOP-->
 		<header class="header-desktop3 d-none d-lg-block">
 			<div class="section__content section__content--p35">
 				<div class="header3-wrap">
 					<div class="header__logo">
-						<a href="<?php echo base_url();?>home">
+						<a href="<?php echo base_url(); ?>home">
 							<!-- <h1 style="color:#2C0CB8"> RF&A </h1> -->
 							<img src="<?php echo base_url(); ?>public/images/icon/logo_completa.png" alt="" />
 						</a>
@@ -72,108 +68,37 @@
 									<i class="fas fa-envelope "></i>
 									<span class="bot-line"></span>Email</a>
 							</li> -->
-							<!-- <li>
-                                <a href="table.html">
-                                    <i class="fas fa-trophy"></i>
-                                    <span class="bot-line"></span>Features</a>
-                            </li>
-                            <li class="has-sub">
-                                <a href="#">
-                                    <i class="fas fa-copy"></i>
-                                    <span class="bot-line"></span>Pages</a>
-                                <ul class="header3-sub-list list-unstyled">
-                                    <li>
-                                        <a href="login.html">Login</a>
-                                    </li>
-                                    <li>
-                                        <a href="register.html">Register</a>
-                                    </li>
-                                    <li>
-                                        <a href="forget-pass.html">Forget Password</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="has-sub">
-                                <a href="#">
-                                    <i class="fas fa-desktop"></i>
-                                    <span class="bot-line"></span>UI Elements</a>
-                                <ul class="header3-sub-list list-unstyled">
-                                    <li>
-                                        <a href="button.html">Button</a>
-                                    </li>
-                                    <li>
-                                        <a href="badge.html">Badges</a>
-                                    </li>
-                                    <li>
-                                        <a href="tab.html">Tabs</a>
-                                    </li>
-                                    <li>
-                                        <a href="card.html">Cards</a>
-                                    </li>
-                                    <li>
-                                        <a href="alert.html">Alerts</a>
-                                    </li>
-                                    <li>
-                                        <a href="progress-bar.html">Progress Bars</a>
-                                    </li>
-                                    <li>
-                                        <a href="modal.html">Modals</a>
-                                    </li>
-                                    <li>
-                                        <a href="switch.html">Switchs</a>
-                                    </li>
-                                    <li>
-                                        <a href="grid.html">Grids</a>
-                                    </li>
-                                    <li>
-                                        <a href="fontawesome.html">FontAwesome</a>
-                                    </li>
-                                    <li>
-                                        <a href="typo.html">Typography</a>
-                                    </li> -->
+							
 						</ul>
-						</li>
-						</ul>
+ 						</ul>
 					</div>
 					<div class="header__tool">
 
 						<div class="header-button-item js-item-menu">
-							  <div class="header-button-item has-noti js-item-menu">  
-							<i class="zmdi zmdi-notifications"></i>
-							<div class="notifi-dropdown notifi-dropdown--no-bor js-dropdown">
-							 
-								<div class="notifi__item">
-									<div class="bg-c1 img-cir img-40">
-										<i class="zmdi zmdi-email-open"></i>
-									</div>
-									<div class="content">
-										<p>Bem-vindo(a) ao novo CRM da Ricardo Furtado & Associados!</p>
-										<span class="date"><?php
-								    date_default_timezone_set('America/Sao_Paulo');
-								echo date('H:i:s / d-m-Y ');?></span>
-									</div>
-								</div>
- 
+							<div class="header-button-item has-noti js-item-menu">
+								<i class="zmdi zmdi-notifications"></i>
+								<div class="notifi-dropdown notifi-dropdown--no-bor js-dropdown">
 
+									<div class="notifi__item">
+										<div class="bg-c1 img-cir img-40">
+											<i class="zmdi zmdi-email-open"></i>
+										</div>
+										<div class="content">
+											<p>Bem-vindo(a) ao novo CRM da Ricardo Furtado & Associados!</p>
+											<span class="date"><?php echo date('H:i:s / d-m-Y '); ?></span>
+										</div>
+									</div>
+
+								</div>
 							</div>
 						</div>
 
 						<div class="header-button-item js-item-menu">
-							<!-- <i class="zmdi zmdi-settings"></i>
+							<i class="zmdi zmdi-settings"></i>
 							<div class="setting-dropdown js-dropdown">
 								<div class="account-dropdown__body">
 									<div class="account-dropdown__item">
-										<a href="#">
-											<i class="zmdi zmdi-account"></i>Conta</a>
-									</div>
-									<div class="account-dropdown__item">
-										<a href="#">
-											<i class="zmdi zmdi-settings"></i>Configurações</a>
-									</div>
-								</div><div class="setting-dropdown js-dropdown">
-								<div class="account-dropdown__body">
-									<div class="account-dropdown__item">
-										<a href="#">
+										<a href="<?php echo base_url(); ?>C_perfil/">
 											<i class="zmdi zmdi-account"></i>Conta</a>
 									</div>
 									<div class="account-dropdown__item">
@@ -181,52 +106,62 @@
 											<i class="zmdi zmdi-settings"></i>Configurações</a>
 									</div>
 								</div>
-							 
-								<div class="account-dropdown__body">
-									<div class="account-dropdown__item">
-										<a href="#">
-											<i class="zmdi zmdi-globe"></i>Language</a>
-									</div>
-									<div class="account-dropdown__item">
-										<a href="#">
-											<i class="zmdi zmdi-pin"></i>Location</a>
-									</div>
-									<div class="account-dropdown__item">
-										<a href="#">
-											<i class="zmdi zmdi-email"></i>Email</a>
-									</div>
-									<div class="account-dropdown__item">
-										<a href="#">
-											<i class="zmdi zmdi-notifications"></i>Notifications</a>
-									</div>
+								<!-- <div class="setting-dropdown js-dropdown">
+									<div class="account-dropdown__body">
+										<div class="account-dropdown__item">
+											<a href="#">
+												<i class="zmdi zmdi-account"></i>Conta</a>
+										</div>
+										<div class="account-dropdown__item">
+											<a href="#">
+												<i class="zmdi zmdi-settings"></i>Configurações</a>
+										</div>
+									</div> -->
+									<!--
+									<div class="account-dropdown__body">
+										<div class="account-dropdown__item">
+											<a href="#">
+												<i class="zmdi zmdi-globe"></i>Language</a>
+										</div>
+										<div class="account-dropdown__item">
+											<a href="#">
+												<i class="zmdi zmdi-pin"></i>Location</a>
+										</div>
+										<div class="account-dropdown__item">
+											<a href="#">
+												<i class="zmdi zmdi-email"></i>Email</a>
+										</div>
+										<div class="account-dropdown__item">
+											<a href="#">
+												<i class="zmdi zmdi-notifications"></i>Notifications</a>
+										</div>
+								</div> 
 								</div> -->
-							</div> 
+							</div>
 						</div>
 						<div class="account-wrap">
 							<div class="account-item account-item--style2 clearfix js-item-menu">
 								<div class="image">
-									<img src="<?php echo base_url(); ?>public/images/perfil/<?php echo $this->session->userdata('usuario_nome');?>.jpg"
-										alt="John Doe" />
+									<img src="<?php echo base_url(); ?>public/images/perfil/<?php echo $this->session->userdata('usuario_nome'); ?>.jpg" alt="John Doe" />
 								</div>
 								<div class="content">
-									<a class="js-acc-btn" href="<?php echo site_url('C_login')?>"> <?php echo $this->session->userdata('usuario_nome');?> </a>
+									<a class="js-acc-btn" href="<?php echo site_url('C_login') ?>"> <?php echo $this->session->userdata('usuario_nome'); ?> </a>
 								</div>
 								<div class="account-dropdown js-dropdown">
 									<div class="info clearfix">
 										<div class="image">
 											<a href="#">
 												<!-- imagem de perfil, alterar agora-->
-												<img src="<?php echo base_url(); ?>public/images/perfil/<?php echo $this->session->userdata('usuario_nome');?>.jpg"
-													alt="John Doe" />
+												<img src="<?php echo base_url(); ?>public/images/perfil/<?php echo $this->session->userdata('usuario_nome'); ?>.jpg" alt="John Doe" />
 											</a>
 
 											<!-- checkpoint -->
 										</div>
 										<div class="content">
 											<h5 class="name">
-												<a href="#"><?php echo $this->session->userdata('usuario_nome');?> </a>
+												<a href="#"><?php echo $this->session->userdata('usuario_nome'); ?> </a>
 											</h5>
-											<span class="email"><?php echo $this->session->userdata('usuario_email');?> </span>
+											<span class="email"><?php echo $this->session->userdata('usuario_email'); ?> </span>
 										</div>
 									</div>
 									<div class="account-dropdown__body">
@@ -241,13 +176,13 @@
 										<div class="account-dropdown__item" data-toggle="tooltip" data-placement="left" title="Acesse apenas na presença de um Tecnico, por favor!">
 											<a href="#">
 												<i class="zmdi zmdi-money-box"></i>Historico</a>
-                                        </div>
-                                        <div class="account-dropdown__footer">
-										<a href="<?php echo site_url('C_login/logout');?>">
-											<i class="zmdi zmdi-power"></i>Sair</a>
+										</div>
+										<div class="account-dropdown__footer">
+											<a href="<?php echo site_url('C_login/logout'); ?>">
+												<i class="zmdi zmdi-power"></i>Sair</a>
+										</div>
 									</div>
-									</div>
-								
+
 								</div>
 							</div>
 						</div>
@@ -262,29 +197,29 @@
 			<div class="header-mobile__bar">
 				<div class="container-fluid">
 					<div class="header-mobile-inner">
-						<a href="<?php echo base_url();?>home">
+						<a href="<?php echo base_url(); ?>home">
 							<!-- <h1 style="color:#2C0CB8"> RF&A </h1> -->
 							<img src="<?php echo base_url(); ?>public/images/icon/logo_completa.png" alt="" />
 						</a>
-						
+
 						</a>
-						<button class="hamburger hamburger--slider"  type="button">
+						<button class="hamburger hamburger--slider" type="button">
 							<span class="hamburger-box" ">
-								<span class="hamburger-inner" ></span>
+								<span class=" hamburger-inner"></span>
 							</span>
 						</button>
 					</div>
 				</div>
 			</div>
-			<nav class="navbar-mobile"  style="padding-top:20px;">
+			<nav class="navbar-mobile" style="padding-top:20px;">
 				<div class="container-fluid">
-					<ul class="navbar-mobile__list list-unstyled" >
+					<ul class="navbar-mobile__list list-unstyled">
 						<li class="has-sub">
 							<a class="js-arrow" style="color:black;" href="#">
 								<i class="fas fa-tachometer-alt"></i>Dashboard</a>
 							<ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
 								<li>
-									<a href="<?php echo base_url();?>/home">Dashboard</a>
+									<a href="<?php echo base_url(); ?>/home">Dashboard</a>
 								</li>
 								<!-- <li>
 									<a href="index2.html">Dashboard 2</a>
@@ -369,8 +304,8 @@
 								<li>
 									<a href="typo.html">Typography</a>
 								</li> -->
-							</ul>
-						</li>
+					</ul>
+					</li>
 					</ul>
 				</div>
 			</nav>
@@ -380,7 +315,7 @@
 				<div class="header-button-item has-noti js-item-menu">
 					<i class="zmdi zmdi-notifications"></i>
 					<div class="notifi-dropdown notifi-dropdown--no-bor js-dropdown">
-					 
+
 						<div class="notifi__item">
 							<div class="bg-c1 img-cir img-40">
 								<i class="zmdi zmdi-email-open"></i>
@@ -388,11 +323,11 @@
 							<div class="content">
 								<p>Bem vindo ao novo CRM da RF&A!!</p>
 								<span class="date"><?php
-								    date_default_timezone_set('America/Sao_Paulo');
-								echo date('H:i:s / d-m-Y ');?></span>
+													date_default_timezone_set('America/Sao_Paulo');
+													echo date('H:i:s / d-m-Y '); ?></span>
 							</div>
 						</div>
-						 
+
 					</div>
 				</div>
 				<div class="header-button-item js-item-menu">
@@ -443,7 +378,7 @@
 				<div class="account-wrap">
 					<div class="account-item account-item--style2 clearfix js-item-menu">
 						<div class="image">
-							<img src="<?php echo base_url(); ?>public/images/perfil/<?php echo $this->session->userdata('usuario_nome');?>.jpg" alt="John Doe" />
+							<img src="<?php echo base_url(); ?>public/images/perfil/<?php echo $this->session->userdata('usuario_nome'); ?>.jpg" alt="John Doe" />
 						</div>
 						<div class="content">
 							<a class="js-acc-btn" href="#">Johann Kaltner</a>
@@ -452,15 +387,14 @@
 							<div class="info clearfix">
 								<div class="image">
 									<a href="#">
-										<img src="<?php echo base_url(); ?>public/images/perfil/<?php echo $this->session->userdata('usuario_nome');?>.jpg"
-											alt="John Doe" />
+										<img src="<?php echo base_url(); ?>public/images/perfil/<?php echo $this->session->userdata('usuario_nome'); ?>.jpg" alt="John Doe" />
 									</a>
 								</div>
 								<div class="content">
 									<h5 class="name">
-										<a href="#"><?php echo $this->session->userdata('usuario_nome');?></a>
+										<a href="#"><?php echo $this->session->userdata('usuario_nome'); ?></a>
 									</h5>
-									<span class="email"><?php echo $this->session->userdata('usuario_email');?> </span>
+									<span class="email"><?php echo $this->session->userdata('usuario_email'); ?> </span>
 								</div>
 							</div>
 							<div class="account-dropdown__body">
@@ -478,7 +412,7 @@
 								</div>
 							</div>
 							<div class="account-dropdown__footer">
-							<a href="<?php echo site_url('C_login/logout');?>">
+								<a href="<?php echo site_url('C_login/logout'); ?>">
 									<i class="zmdi zmdi-power"></i>Logout</a>
 							</div>
 						</div>
