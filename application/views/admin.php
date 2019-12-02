6d1b48e56a5d7252b778dabeee824900e4fce57d
johannkaltner <johannkaltnerdev@gmail.com>
2019-12-02 17:31:32 -0300

    truncate table no sysadmin

--- application/views/admin.php
+++ application/views/admin.php
@@ -121,13 +121,16 @@
 
     <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
       <h3>Menu 1</h3>
-      <p>Some content here.</p>
     </div>
 
     <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
       <h3>Menu 2</h3>
-      <p>Some content here.</p>
-    </div>
+      <div>
+        <a type="button" style="color:red">Apagar Banco de Dados</a>
+      </div>
+    
+  </div>
+
 
   </div>
 </div>
