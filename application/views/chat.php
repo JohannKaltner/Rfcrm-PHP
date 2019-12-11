b899f1ba5bb8ebff593c2689e8e3ea152a3699a2
Johann <johannkaltnerdev@gmail.com>
2019-12-11 17:26:40 -0300

    commit automatizado

---
+++ application/views/chat.php
@@ -0,0 +1,83 @@
+<div class="page-content--bgf7">
+	<!-- BREADCRUMB-->
+	<section class="au-breadcrumb2">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="au-breadcrumb-content">
+						<div class="au-breadcrumb-left">
+							<span class="au-breadcrumb-span">Você está aqui:</span>
+							<ul class="list-unstyled list-inline au-breadcrumb__list">
+								<li class="list-inline-item active">
+									<a href="<?php echo base_url(); ?>Home">Home</a>
+								</li>
+								<li class="list-inline-item seprate">
+									<span>/</span>
+								</li>
+								<li class="list-inline-item">Chat
+
+								</li>
+							</ul>
+						</div>
+						<form class="au-form-icon--sm" action="" method="post">
+							<input class="au-input--w300 au-input--style2" type="text"
+								placeholder="procure por Clientes &amp; Chamados...">
+							<button class="au-btn--submit2" type="submit">
+								<i class="zmdi zmdi-search"></i>
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- END BREADCRUMB-->
+
+	<!-- WELCOME-->
+	<section class="welcome p-t-10">
+		<div class="container">
+			<!-- <div class="row">
+				<div class="col-md-12">
+					<h1 class="title-4">Bem-vindo,
+
+						 ADICIONAR O GET_NOME / GET_SETOR 
+								    <span><?php echo $this->session->userdata('usuario_nome'); ?>!</span> 
+						  <small> do Setor :<?php echo $this->session->userdata('usuario_setor'); ?></small>  
+					<hr class="line-seprate">
+				</div>	
+			</div>  
+  END WELCOME-->
+		</div>
+	</section>
+
+	<!-- STATISTIC-->
+	<section class="statistic statistic2">
+		<div class="container">
+			<div class="col-md-3" style="border:1px solid grey">
+				<div class="row">
+					<ul>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						<li><a href="#"> Johann</a></li>
+						
+					</ul>
+				</div>
+
+
+
+			</div>
+
+		</div>
+	</section>
+
+</div>
